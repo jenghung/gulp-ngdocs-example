6243825561c7888d4b95dab3edd64b6434d2fc49
jenghung <jenghung@cht.com.tw>
2015-08-24 15:55:03 +0800

    ngdocs, gh-pages gulp sample.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
 var gulp = require('gulp');
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var gulpDocs = require('gulp-ngdocs');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('ngdocs', [], function () {
-  var options = {
-    scripts: ['src.js'],
-    html5Mode: false
-  }
+    var options = {
+        scripts: ['src/src.js'],
+        html5Mode: false
+    }
 
-  var gulpDocs = require('gulp-ngdocs');
-  return gulp.src('src.js')
+    return gulp.src('src/*.js')
     .pipe(gulpDocs.process(options))
     .pipe(gulp.dest('./docs'));
 });
 
+gulp.task('deploy', function() {
+    return gulp.src('docs/**/*')
+    .pipe(ghPages());
+});
+
 gulp.task('default', ['ngdocs'], function (cb) {
-  var app = connect().use(serveStatic('./docs'));
-  app.listen(8000);
-  cb();
-  console.log('Server started on http://localhost:8000');
+    var app = connect().use(serveStatic('./docs'));
+    app.listen(8000);
+    cb();
+    console.log('Server started on http://localhost:8000');
 });
 
