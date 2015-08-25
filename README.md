af8a6da0db1c0012f643776b9217ecf8ad276a71
jenghung <jenghung@cht.com.tw>
2015-08-25 13:39:22 +0800

    Update 2015-08-25T05:39:20.610Z

--- README.md
+++
@@ -1,9 +0,0 @@
-## Dependencies
-
-* install [node](http://nodejs.org)
-* install [gulp](https://github.com/gulpjs/gulp) `npm install -g gulp`
-* install dependencies: `npm install`
-
-## Run
-
-Run `gulp` and open http://localhost:8000 in your Browser.
---
+++ js/docs-setup.js
@@ -0,0 +1,60 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "c.focus.directive:cFocusOn",
+      "shortName": "cFocusOn",
+      "type": "directive",
+      "moduleName": "c.focus",
+      "shortDescription": "當cFocusOn 的變數名稱為true時，則focus進入該欄位。",
+      "keywords": "$scope $timeout angular api app c-focus-on cfocuson clickme controller data-ng-click directive focus focusme function input input1 inputval module ng-controller ng-model test text true type var"
+    },
+    {
+      "section": "api",
+      "id": "rfx.directive:rAutogrow",
+      "shortName": "rAutogrow",
+      "type": "directive",
+      "moduleName": "rfx",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically class content directive html input-block-level module ng-model r-autogrow resize rfx size text textarea"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/jquery.min.js",
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js",
+      "js/src.js",
+      "js/focus.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {}
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "jquery.min.js",
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js",
+    "src.js",
+    "focus.js"
+  ]
+};
